2c5f55f48640c33bfb4d27fa2e0e0b4cee747124
logahofm168 <logahofm168@SDS-L-H9QLG63>
2024-11-26 11:47:22 -0500

    added stuff

--- Greeting Card/Form1.cs
+++ Greeting Card/Form1.cs
@@ -19,7 +19,7 @@ namespace Greeting_Card
 
         private void Form1_Shown(object sender, EventArgs e)
         {
-
+            // hold values for initial design and displays them  
 
             Graphics g = this.CreateGraphics();
 
@@ -39,20 +39,37 @@ namespace Greeting_Card
 
         private void Form1_Click(object sender, EventArgs e)
         {
+            // 
+
             Graphics g = this.CreateGraphics();
 
             Font drawfont = new Font("Baskerville Old Face", 50, FontStyle.Bold);
             SolidBrush drawBrush = new SolidBrush(Color.White);
 
             Pen orangePen = new Pen(Color.Orange, 5);
-            SolidBrush orangeBrush = new SolidBrush(Color.Black);
+            SolidBrush orangeBrush = new SolidBrush(Color.Orange);
+            Pen darkOrangePen = new Pen(Color.DarkOrange);
+            SolidBrush darkOrangeBrush = new SolidBrush(Color.DarkOrange);
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
 
             g.Clear(Color.Black);
 
             g.DrawString("Happy", drawfont, drawBrush, 115, 100);
             g.DrawString("Halloween", drawfont, drawBrush, 67, 200);
 
-            g.DrawEllipse(orangePen, 120, 445, 55, 100);
+            g.FillEllipse(orangeBrush, 130, 405, 55, 95);
+            g.FillEllipse(darkOrangeBrush, 140, 400, 50, 105);
+            g.FillEllipse(orangeBrush, 155, 395, 45, 115);
+            g.FillEllipse(orangeBrush, 200, 405, 55, 95);
+            g.FillEllipse(darkOrangeBrush, 195, 400, 50, 105);
+            g.FillEllipse(orangeBrush, 175, 395, 55, 115);
+            g.FillEllipse(darkOrangeBrush, 170, 390, 45, 125);
+
+            g.FillPie(blackBrush, 140, 395, 45, 45, 60, 55);
+            g.FillPie(blackBrush, 190, 395, 45, 45, 60, 55);
+            g.FillEllipse(blackBrush, 145, 460, 100, 25);
+
+
         }
     }
 }
