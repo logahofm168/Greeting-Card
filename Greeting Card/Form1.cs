198f99fb602508954c6a20275871a91a321ec682
logahofm168 <logahofm168@SDS-L-H9QLG63>
2024-11-27 11:39:00 -0500

    added an animation

--- Greeting Card/Form1.cs
+++ Greeting Card/Form1.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Greeting_Card
@@ -39,7 +39,7 @@ namespace Greeting_Card
 
         private void Form1_Click(object sender, EventArgs e)
         {
-            // 
+            // holds and displays the values for the second page
 
             Graphics g = this.CreateGraphics();
 
@@ -51,6 +51,8 @@ namespace Greeting_Card
             Pen darkOrangePen = new Pen(Color.DarkOrange);
             SolidBrush darkOrangeBrush = new SolidBrush(Color.DarkOrange);
             SolidBrush blackBrush = new SolidBrush(Color.Black);
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+            SolidBrush whiteBrush = new SolidBrush(Color.White);
 
             g.Clear(Color.Black);
 
@@ -65,10 +67,35 @@ namespace Greeting_Card
             g.FillEllipse(orangeBrush, 175, 395, 55, 115);
             g.FillEllipse(darkOrangeBrush, 170, 390, 45, 125);
 
-            g.FillPie(blackBrush, 140, 395, 45, 45, 60, 55);
-            g.FillPie(blackBrush, 190, 395, 45, 45, 60, 55);
-            g.FillEllipse(blackBrush, 145, 460, 100, 25);
+            g.FillPie(blackBrush, 145, 395, 45, 45, 60, 55);
+            g.FillPie(blackBrush, 195, 395, 45, 45, 60, 55);
+            g.FillEllipse(blackBrush, 150, 460, 90, 20);
+            g.FillPie(darkOrangeBrush, 170, 445, 45, 45, 60, 55);
 
+            g.FillPie(redBrush, 137, 285, 105, 110, 60, 55);
+            g.FillEllipse(whiteBrush, 175, 325, 27, 25);
+            g.FillEllipse(whiteBrush, 145, 385, 30, 25);
+            g.FillEllipse(whiteBrush, 165, 385, 25, 25);
+            g.FillEllipse(whiteBrush, 175, 385, 25, 25);
+            g.FillEllipse(whiteBrush, 185, 385, 25, 25);
+            g.FillEllipse(whiteBrush, 195, 385, 25, 25);
+            g.FillEllipse(whiteBrush, 210, 385, 30, 25);
+
+            int cirical1Y = -100;
+            int cirical2Y = -75;
+
+            while (cirical1Y < 500 && cirical2Y < 500)
+            {
+               g.FillEllipse(blackBrush, 30, cirical1Y, 25, 25);
+               cirical1Y += 10;
+               g.FillEllipse(whiteBrush, 60, cirical1Y, 25, 25);
+
+               g.FillEllipse(blackBrush, 145, cirical2Y, 25, 25);
+               cirical2Y += 10;
+               g.FillEllipse(whiteBrush, 60, cirical2Y, 25, 25);
+
+                Thread.Sleep(20);
+            }
 
         }
     }
