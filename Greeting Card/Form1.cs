d8d9bff2f4053057ea7a0f44b83188a1a4d6074b
logahofm168 <logahofm168@SDS-L-H9QLG63>
2024-12-03 11:13:43 -0500

    added sounds

--- Greeting Card/Form1.cs
+++ Greeting Card/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Windows.Forms;
+using System.Media;
 
 namespace Greeting_Card
 {
@@ -15,6 +16,7 @@ namespace Greeting_Card
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void Form1_Shown(object sender, EventArgs e)
@@ -40,6 +42,9 @@ namespace Greeting_Card
         private void Form1_Click(object sender, EventArgs e)
         {
             // holds and displays the values for the second page
+
+            SoundPlayer soundPlayer = new SoundPlayer(Properties.Resources.juskiddink__jingle_bells);
+            soundPlayer.Play();
 
             Graphics g = this.CreateGraphics();
 
@@ -84,19 +89,65 @@ namespace Greeting_Card
             int cirical1Y = -100;
             int cirical2Y = -75;
 
-            while (cirical1Y < 500 && cirical2Y < 500)
+            int cirical3Y = -100;
+            int cirical4Y = -75;
+
+            int cirical5Y = -100;
+            int cirical6Y = -75;
+
+            while (cirical1Y < 500 && cirical2Y < 500 && cirical3Y < 500 && cirical4Y < 500)
             {
-               g.FillEllipse(blackBrush, 30, cirical1Y, 25, 25);
-               cirical1Y += 10;
-               g.FillEllipse(whiteBrush, 60, cirical1Y, 25, 25);
 
-               g.FillEllipse(blackBrush, 145, cirical2Y, 25, 25);
-               cirical2Y += 10;
-               g.FillEllipse(whiteBrush, 60, cirical2Y, 25, 25);
 
-                Thread.Sleep(20);
+                cirical1Y += 10;
+                cirical2Y += 10;
+                cirical3Y += 10;
+                cirical4Y += 10;
+                cirical5Y += 10;
+                cirical6Y += 10;
+                g.FillEllipse(whiteBrush, 30, cirical1Y, 25, 25);
+                g.FillEllipse(whiteBrush, 100, cirical2Y, 25, 25);
+                g.FillEllipse(whiteBrush, 185, cirical3Y, 25, 25);
+                g.FillEllipse(whiteBrush, 260, cirical4Y, 25, 25);
+                g.FillEllipse(whiteBrush, 300, cirical5Y, 25, 25);
+                g.FillEllipse(whiteBrush, 330, cirical6Y, 25, 25);
+
+
+                Thread.Sleep(40);
+
+                g.FillEllipse(blackBrush, 30, cirical1Y, 25, 25);
+                g.FillEllipse(blackBrush, 100, cirical2Y, 25, 25);
+                g.FillEllipse(blackBrush, 185, cirical3Y, 25, 25);
+                g.FillEllipse(blackBrush, 260, cirical4Y, 25, 25);
+                g.FillEllipse(blackBrush, 300, cirical5Y, 25, 25);
+                g.FillEllipse(blackBrush, 330, cirical6Y, 25, 25);
+
+                g.DrawString("Happy", drawfont, drawBrush, 115, 100);
+                g.DrawString("Halloween", drawfont, drawBrush, 67, 200);
+
+                g.FillEllipse(orangeBrush, 200, 405, 55, 95);
+                g.FillEllipse(darkOrangeBrush, 195, 400, 50, 105);
+                g.FillEllipse(orangeBrush, 175, 395, 55, 115);
+                g.FillEllipse(darkOrangeBrush, 170, 390, 45, 125);
+
+                g.FillPie(blackBrush, 145, 395, 45, 45, 60, 55);
+                g.FillPie(blackBrush, 195, 395, 45, 45, 60, 55);
+                g.FillEllipse(blackBrush, 150, 460, 90, 20);
+                g.FillPie(darkOrangeBrush, 170, 445, 45, 45, 60, 55);
+
+                g.FillPie(redBrush, 137, 285, 105, 110, 60, 55);
+                g.FillEllipse(whiteBrush, 175, 325, 27, 25);
+                g.FillEllipse(whiteBrush, 145, 385, 30, 25);
+                g.FillEllipse(whiteBrush, 165, 385, 25, 25);
+                g.FillEllipse(whiteBrush, 175, 385, 25, 25);
+                g.FillEllipse(whiteBrush, 185, 385, 25, 25);
+                g.FillEllipse(whiteBrush, 195, 385, 25, 25);
+                g.FillEllipse(whiteBrush, 210, 385, 30, 25);
+
             }
 
+            SoundPlayer soundPlayer2 = new SoundPlayer(Properties.Resources.single_jingle_bells_shaking);
+            soundPlayer2.Play();
         }
     }
 }
--- Greeting Card/Properties/Resources.Designer.cs
+++ Greeting Card/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Greeting_Card.Properties
-{
-
-
+namespace Greeting_Card.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,80 @@ namespace Greeting_Card.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Greeting_Card.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream _333845__thebillinator3000__spookynoise1 {
+            get {
+                return ResourceManager.GetStream("_333845__thebillinator3000__spookynoise1", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream jingle_stick {
+            get {
+                return ResourceManager.GetStream("jingle_stick", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream juskiddink__jingle_bells {
+            get {
+                return ResourceManager.GetStream("juskiddink__jingle_bells", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream single_jingle_bells_shaking {
+            get {
+                return ResourceManager.GetStream("single_jingle_bells_shaking", resourceCulture);
             }
         }
     }
